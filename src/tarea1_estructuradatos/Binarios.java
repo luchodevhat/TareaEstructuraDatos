7ef691c4cce8edc30d3d96b791c80b07be8f415f
Kendal0924 <84550913+Kendal0924@users.noreply.github.com>
2022-09-29 22:45:27 -0600

    Binarios

---
+++ src/tarea1_estructuradatos/Binarios.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea1_estructuradatos;
+
+/**
+ *
+ * @author Kendal Angulo
+ */
+public class Binarios {
+    
+}
