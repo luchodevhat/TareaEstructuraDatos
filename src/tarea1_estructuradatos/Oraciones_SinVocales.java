5fac6d955fb7a11e5a5fec01415aec917d97c97d
Luis Alejandro <alfaroalejandro597@gmail.com>
2022-10-01 17:01:12 -0600

    ejercicio binarios arreglado

--- src/tarea1_estructuradatos/Oraciones_SinVocales.java
+++ src/tarea1_estructuradatos/Oraciones_SinVocales.java
@@ -3,7 +3,6 @@ package tarea1_estructuradatos;
 import java.util.Scanner;
 
 // a. Elimine las vocales de una oración e imprima la oración resultante.
-
 public class Oraciones_SinVocales {
 
     public void Imprime_SinVocales() {
@@ -11,16 +10,13 @@ public class Oraciones_SinVocales {
         System.out.println("Digite una frase ");
         Scanner consola = new Scanner(System.in);
         String frase = consola.nextLine();
+        
         System.out.println("La frase sin modificar es: " + frase);
+        
         String fraseCons = frase.replaceAll("[a,á,e,é,i,í,o,ó,u,ú, A, E, I, O, U,"
                 + "Á, É, Í, Ó, Ú]", "");
         System.out.println("La frase sin vocales es: " + fraseCons);
+        
         String fraseInv = "";
-        for (int i = fraseCons.length() - 1; i >= 0; i--) {
-            fraseInv = fraseInv + fraseCons.charAt(i);
-        }
-        System.out.println("La frase invertida y sin vocales es: " + fraseInv);
-
     }
 }
-
