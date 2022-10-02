b89d88b94d0d93981eb8e1822e9fd5bb3d5957bb
fercho-05 <fperezalpizar@gmail.com>
2022-10-02 17:00:30 -0600

    Ejercicio B terminado

--- src/tarea1_estructuradatos/SumaNumNaturales.java
+++ src/tarea1_estructuradatos/SumaNumNaturales.java
@@ -5,11 +5,5 @@ package tarea1_estructuradatos;
  * @author Fernando
  */
 public class SumaNumNaturales {
-    public int sumasR(int N){
-        if (N == 1) {
-            return 1;
-        }else{
-            return N + sumasR(N-1);
-        }
-    }
+    
 }
